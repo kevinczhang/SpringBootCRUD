1a7cf773117f038d8de4f34811c013a17ece4b14
Josh <chaozhang78@icloud.com>
2018-04-16 22:09:09 -0400

    Added initial database script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO app_role (id, role_name, description) VALUES (1, 'STANDARD_USER', 'Standard User - Has no admin rights');
+INSERT INTO app_role (id, role_name, description) VALUES (2, 'ADMIN_USER', 'Admin User - Has permission to perform admin tasks');
